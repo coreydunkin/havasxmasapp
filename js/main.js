0ddc194b9b65efd3d37b4dda91314b4b94fbb4e0
coreydunkin <corey.dunkin@gmail.com>
2014-11-12 01:23:14 +1100

    added object to hold repeated background image

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ app.controller( 'MainCtrl', function( $scope, $interval, $timeout, $window, $htt
   $scope.backimgOne = '/img/patterns/pattern1-preview.jpg';
   $scope.backimgTwo = '/img/patterns/pattern2-preview.jpg';
   $scope.backimgThree = '/img/patterns/pattern3-preview.jpg';
+  $scope.imageTemplate = [];
 
   // FUNCTION FOR SLIDE ONE
   $scope.controllerSlideOne = function() {
@@ -28,58 +29,116 @@ app.controller( 'MainCtrl', function( $scope, $interval, $timeout, $window, $htt
   // FUNCTION FOR SLIDE TWO
   $scope.controllerSlideTwo = function() {
     console.info('Slide Two');
-    
+    // Define imageTemplate
+    $scope.imageTemplate = [];
+    console.log($scope.imageTemplate);
+
+    // Fadein transition for all pattern choices
     $('.image-choice.one')
     .removeClass('fadeOut')
-    .addClass('animated fadeIn');
+    .addClass('animated bounceInUp');
     
     $('.image-choice.two')
-	.removeClass('fadeOut')
-    .addClass('animated fadeIn');
+	  .removeClass('fadeOut')
+    .addClass('animated bounceInUp');
     
     $('.image-choice.three')
-	.removeClass('fadeOut')
-    .addClass('animated fadeIn');
+	  .removeClass('fadeOut')
+    .addClass('animated bounceInUp');
 
+    // On mouseenter and mouseleave show info
     $('.image-choice').on('mouseenter', function() {
     	var infoBox = $(this).find('.info');
     	var infoBoxActive = $(this).find('.info.active');    	
-		TweenLite.to(infoBox, 0.2, {bottom:"0px", ease:Linear.easeOut});
+		  TweenLite.to(infoBox, 0.2, {bottom:"0px", ease:Linear.easeOut});
     });
     $('.image-choice').on('mouseleave', function() {
     	var infoBox = $(this).find('.info');
     	var infoBoxActive = $(this).find('.info.active');
-		TweenLite.to(infoBox, 0.2, {bottom:"-100px", ease:Linear.easeOut});
+		  
+      if ($(infoBox).find('.tick-container .icon').hasClass('active')) {
+        TweenLite.to(infoBox, 0.2, {bottom:"0px", ease:Linear.easeOut});
+      } else {
+        TweenLite.to(infoBox, 0.2, {bottom:"-100px", ease:Linear.easeOut});
+      }
     });
 
+    // Choose pattern and apply to array for pattern
     $('.image-choice').unbind('click').bind('click', function() {
-    	console.log('click');
+
     	var tick = $(this).find('.info .icon');
     	var info = $(this).find('.info');
-    	if ($(tick).hasClass('active')) {
+    	
+      if ($(this).hasClass('active')) {
+        // Clear pattern selection
+        $scope.imageTemplate = [];
+        console.log($scope.imageTemplate);
+
+        $(this).removeClass('active');
+
+      } else {
+        
+        $('.image-choice').removeClass('active');
+        $(this).addClass('active');
+        
+
+        // Make pattern selection
+        if ($('.image-choice.one').hasClass('active')) {
+          $scope.imageTemplate = '/img/patterns/pattern1-preview.jpg';
+          $scope.imagePattern = '/img/patterns/pattern1.png';
+        }
+        if ($('.image-choice.two').hasClass('active')) {
+          $scope.imageTemplate = '/img/patterns/pattern2-preview.jpg';
+          $scope.imagePattern = '/img/patterns/pattern2.png';
+        }
+        if ($('.image-choice.three').hasClass('active')) {
+          $scope.imageTemplate = '/img/patterns/pattern3-preview.jpg';
+          $scope.imagePattern = '/img/patterns/pattern3.png';
+        }
+        console.log($scope.imageTemplate);
+
+      }
+
+      if ($(tick).hasClass('active')) {
 			
+      $('.image-choice .info .icon')
+      .removeClass('active animated bounceIn')
+      .addClass('hidden');
+
 			$(tick)
 			.removeClass('active animated bounceIn')
 			.addClass('hidden');
+
     	} else {
-
+        TweenLite.to('.image-choice .info', 0.2, {bottom:"-100px", ease:Linear.easeOut});
     		TweenLite.to(info, 0.2, {bottom:"0px", ease:Linear.easeOut});
+        
 
-			$('.image-choice .info .icon')
-			.removeClass('active animated bounceIn')
-			.addClass('hidden');
+  			$('.image-choice .info .icon')
+  			.removeClass('active animated bounceIn')
+  			.addClass('hidden');
 
-			$(tick)
-			.addClass('active animated bounceIn')
-			.removeClass('hidden');
-    	}
+  			$(tick)
+  			.addClass('active animated bounceIn')
+  			.removeClass('hidden');
+      }
+
     });
-
+    // Reset pattern on page three before loading page three
+    $('#thirdPage .row').addClass('hidden').removeClass('animated bounceInLeft');   
   };  
 
   // FUNCTION FOR SLIDE THREE
   $scope.controllerSlideThree = function() {
-    console.info('Slide Three');
+    
+    $timeout(function() {
+      $scope.imagePattern;
+      console.log($scope.imagePattern);
+      // Animate in pagethree
+      $('#thirdPage .row').removeClass('hidden').addClass('animated bounceInLeft');      
+    });
+
+    //$scope.imageTemplate;
   };
 
   // FUNCTION FOR SLIDE FOUR
